052a9185db0d56baedd4bfa5fae492cb8646134c
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2019-12-18 12:34:34 +0000

    Use Devise for user sign up

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -6,7 +6,8 @@ RSpec.feature "Sign up", type: :feature do
     click_link "Sign up"
     fill_in "Email", with: "josh@example.com"
     fill_in "Password", with: "1234567890"
-    click_button "Submit"
-    expect(page).to have_content("You have successfully signed up!")
+    fill_in "Password confirmation", with: "1234567890"
+    click_button "Sign up"
+    expect(page).to have_content("Welcome! You have signed up successfully")
   end
 end
