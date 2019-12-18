65bf83ce004ae4ae904e3a394494ed300600eb43
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2019-12-18 19:08:52 +0000

    Test invalid email and password sign up errors

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -4,12 +4,25 @@ RSpec.feature "Sign up", type: :feature do
   scenario "New user can sign up" do
     visit "/"
     click_link "Sign up"
-    fill_in "Email", with: "josh@example.com"
-    fill_in "Password", with: "1234567890"
-    fill_in "Password confirmation", with: "1234567890"
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
 
     click_button "Sign up"
 
     expect(page).to have_content("Welcome! You have signed up successfully")
+  end
+
+  scenario "Validates email and password" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: "j"
+    fill_in "Password", with: "p"
+    fill_in "Password confirmation", with: "p"
+
+    click_button "Sign up"
+
+    expect(page).to have_content("Email is invalid")
+    expect(page).to have_content("Password is too short")
   end
 end
