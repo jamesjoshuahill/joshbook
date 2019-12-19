a7032081b1842a1d47aab078c694b2f38196f7ac
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2019-12-19 11:52:20 +0000

    Change login -> sign in
    Add feature test

---
+++ spec/features/user_can_sign_in_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "Sign in", type: :feature do
+  scenario "Registered user can sign in" do
+    user = FactoryBot.create(:user)
+    visit "/"
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Sign in"
+
+    expect(page).to have_content("Signed in successfully")
+  end
+end
